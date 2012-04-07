8791fb112c9468ba93d00c4fd3c537a426de1960
Hsu <hmhlaing@hotmail.com>
2012-04-07 14:55:19 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hanabi::Application.config.secret_token = 'b625d231edce3664b21aac865c65c03eb558a8933ec80ec91f999397912ed7b99a3ee1fb976c0c16e963fb20fc9a5b1d6982af20b837dda1c0744b9020ee0338'
