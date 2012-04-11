31241c84e360325b5dadb406dca44f416c7378bd
Hsu <hmhlaing@hotmail.com>
2012-04-11 18:07:14 +0100

    "nothing to say"

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -18,6 +18,7 @@ describe PagesController do
       response.should have_selector("title",
                                     :content => @base_title + "|Home")
     end
+
   end
 
   describe "GET 'contact'" do
